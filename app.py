fb94d9656ea8bc07d7f8ba20803e395d2734332e
Ryan <ryan@ispeakcomputer.com>
2021-03-07 03:26:00 -0700

    added basic authentication system

--- app.py
+++ app.py
@@ -3,17 +3,41 @@ from flask import jsonify, request
 import json
 from bitly import Bitly
 from helper import Helper
-
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
 bitly_object = Bitly()
 helper_object = Helper()
+# Setup the Flask-JWT-Extended extension
+app.config["JWT_SECRET_KEY"] = "super-secret"  # Change this!
+jwt = JWTManager(app)
+
+
+# Create a route to authenticate your users and return JWTs. The
+# create_access_token() function is used to actually generate the JWT.
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+    if username != "test" or password != "test":
+        return jsonify({"msg": "Bad username or password"}), 401
+
+    access_token = create_access_token(identity=username)
+    return jsonify(access_token=access_token)
+
+
+# Protect a route with jwt_required, which will kick out requests
+# without a valid JWT present.
+
 
 @app.route('/', methods=['GET'])
+@jwt_required
 def home():
-    dir(request)
     # Get default user group
     group = bitly_object.group_getter() 
     # Get the links for the user default group
