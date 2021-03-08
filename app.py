8be30350b87bc38a78c7f442ecb89adc3a908f52
Ryan <ryan@ispeakcomputer.com>
2021-03-08 14:46:24 -0700

    checking for bitly token and also if it is working

--- app.py
+++ app.py
@@ -45,16 +45,21 @@ def login():
 def home():
     # Get default user group
     group = bitly_object.group_getter() 
-    # Get the links for the user default group
-    group_links = bitly_object.bitlink_getter(group['default_group_guid'])
-    # Return identifying bitlink along with its countries click data
-    links = helper_object.json_snippet_builder(group_links)
-    # generate 30 day avg from country click data and build dict
-    data = helper_object.avg_calculator(links)
-    
-    return jsonify(data)
+    # Sends user the message that Bitly doesnt like their token.
+    if 'FORBIDDEN' in group.values():
+        return jsonify({'message':'Bitly token is bad, broken or expired.'})
+    else:
+        # Get the links for the user default group
+        group_links = bitly_object.bitlink_getter(group['default_group_guid'])
+        # Return identifying bitlink along with its countries click data
+        links = helper_object.json_snippet_builder(group_links)
+        # generate 30 day avg from country click data and build dict
+        data = helper_object.avg_calculator(links)
+        
+        return jsonify(data)
 
 if __name__ == "__main__":
+    #Checks settings and reports warnings or kills app if the Bitly Token is gone or not valid
     if jwt_username and jwt_password == "test":
         print('\033[33m' + ' * WARNING: Jwt username and password are default. Change this for production.')
         print('\033[39m')
@@ -63,9 +68,16 @@ if __name__ == "__main__":
         print('\033[33m' + ' * WARNING: Jwt Secret is set to default. Change this for production.')
         print('\033[39m')
     if not token:
-        print('\033[31m' + ' * Fatal Error: Bitly API token missing')
+        print('\033[31m' + ' * Error: Bitly API token missing. Exiting.')
         print('\033[39m')
         quit() 
     else:
-        # app.run(host="0.0.0.0", port=80)
-        app.run(host="0.0.0.0")
+        #Check our token exists and that Bitly likes it before starting
+        group = bitly_object.group_getter() 
+        print(group)
+        if 'FORBIDDEN' in group.values():
+            print('\033[31m' + ' * Error: Bitly doesn\'t like your token. Replace or check it. Exiting.')
+            print('\033[39m')
+        else:
+            # app.run(host="0.0.0.0", port=80)
+            app.run(host="0.0.0.0")
