fd2425a97181a526caa5ae0285298b581d1e8093
Ryan <ryan@ispeakcomputer.com>
2021-03-08 12:20:37 -0700

    updating the start_here.sh script

--- start_here.sh
+++ start_here.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-
+sudo pkill -9 python3
 export BITLYTOKEN=
+export JWTUSER=test 
+export JWTPASS=test
+export JWT_SECRET_KEY=changethis
 source env/bin/activate
 python3 app.py
-trap 'pkill -9 python3' EXIT
