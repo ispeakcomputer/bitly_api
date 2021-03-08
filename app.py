bebe4da89c81612a3709edcda67770eb3ac6da37
Ryan <ryan@ispeakcomputer.com>
2021-03-08 11:43:30 -0700

    allow changing JWT user and password with environment vars and alert user if no Bitly API token has been set before exiting

--- app.py
+++ app.py
@@ -9,17 +9,19 @@ from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
 from flask_jwt_extended import JWTManager
 
+jwt_username = os.environ.get('JWTUSER')
+jwt_password = os.environ.get('JWTPASS')
+jwt_secret_key  = os.environ.get('JWT_SECRET_KEY')
+
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
+app.config["JWT_SECRET_KEY"] = jwt_secret_key  # Change this!
 
 bitly_object = Bitly()
 helper_object = Helper()
 # Setup the Flask-JWT-Extended extension
-app.config["JWT_SECRET_KEY"] = "super-secret"  # Change this!
 jwt = JWTManager(app)
 
-jwt_username = os.environ.get('JWTUSER')
-jwt_password = os.environ.get('JWTPASS')
 # Create a route to authenticate your users and return JWTs. The
 # create_access_token() function is used to actually generate the JWT.
 @app.route("/login", methods=["POST"])
@@ -52,5 +54,12 @@ def home():
     return jsonify(data)
 
 if __name__ == "__main__":
+    if jwt_username and jwt_password == "test":
+        print('\033[33m' + ' * WARNING: Jwt username and password are default. Change this for production.')
+        print('\033[39m')
+    if not jwt_secret_key:
+        print('\033[31m' + ' * ERROR: Bitly API key isn\'t set. You must do this first ')
+        print('\033[39m')
+        quit() 
     # app.run(host="0.0.0.0", port=80)
     app.run(host="0.0.0.0")
